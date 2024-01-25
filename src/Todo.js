319ff42e375cbc4dde230c086066b11a4029a7d3
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2024-01-25 16:56:25 +0300

    added a sweet alert for the delete and confirmation

--- src/Todo.js
+++ src/Todo.js
@@ -1,49 +1,74 @@
-import React, {useState} from 'react'
-
+import React, { useState } from 'react';
+import Swal from 'sweetalert2'; // Import SweetAlert
 
 function Todo() {
-    const [tasks, setTasks] = useState("")
-    const [list, setList] = useState([])
+  const [tasks, setTasks] = useState("");
+  const [list, setList] = useState([]);
 
-    function handleInputChange(e){
-       setTasks(e.target.value)
-    }
-    function handleAddTask(todo){
-        const newTask ={
-            id: Math.random(),
-            task : todo
-        }
-        setList([...list, newTask])
-        setTasks("")
-    }
-    function handleDeleteTodo(id) {
-        const shouldDelete = window.confirm("Are you sure you want to delete this?");
-        
-        if (shouldDelete) {
-          const newList = list.filter((todo) => todo.id !== id);
-          setList(newList);
-        }
+  function handleInputChange(e) {
+    setTasks(e.target.value);
+  }
+
+  function handleAddTask(todo) {
+    const newTask = {
+      id: Math.random(),
+      task: todo,
+    };
+    setList([...list, newTask]);
+    setTasks("");
+  }
+
+  function handleDeleteTodo(id) {
+    // Use SweetAlert for confirmation
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover this task!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const newList = list.filter((todo) => todo.id !== id);
+        setList(newList);
+        Swal.fire('Deleted!', 'Your task has been deleted.', 'success');
       }
+    });
+  }
+
   return (
     <>
-        <section id='todo'>
-            <h1>To do list</h1>
-            <div id='input'>
-                <input type='text' placeholder='add item ...'value={tasks} onChange={handleInputChange}/> 
-                <button type='submit' onClick={() => handleAddTask(tasks)}>Add Item</button>
+      <section id='todo'>
+        <h1>To do list</h1>
+        <div id='input'>
+          <input
+            type='text'
+            placeholder='add item ...'
+            value={tasks}
+            onChange={handleInputChange}
+          />
+          <button type='submit' onClick={() => handleAddTask(tasks)}>
+            Add Item
+          </button>
+        </div>
+        <div>
+          {list.map((listitem) => (
+            <div key={listitem.id} id='todolist'>
+              <h2>{listitem.task}</h2>
+              <p>{new Date().toLocaleString()}</p>
+              <button
+                onClick={() => handleDeleteTodo(listitem.id)}
+                id='delete'
+              >
+                Delete
+              </button>
             </div>
-            <div>
-                {list.map((listitem) =>(
-                    <div key={listitem.id} id='todolist'>
-                        <h2>{listitem.task}</h2>
-                        <p>{new Date().toLocaleString()}</p>
-                        <button onClick={() => handleDeleteTodo(listitem.id)} id='delete'>Delete</button>
-                    </div>
-                ))}
-            </div>
-        </section>
+          ))}
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Todo
+export default Todo;
